d4843a8f06714e43739ddb059c30c864cf4d9f0f
Jobaer <jobaer@student.chalmers.se>
2017-10-24 15:32:28 +0300

    PrinterBook to PrintBook

--- PrintBook/src/main/java/g5/printbook/database/Config.java
+++ PrintBook/src/main/java/g5/printbook/database/Config.java
@@ -6,6 +6,6 @@ package g5.printbook.database;
 
 public class Config {
     protected static final String TAG_SUCCESS = "success";
-    protected static final String url_create_project = "http://10.0.2.2:8081/PrinterBook/create_project.php";
+    protected static final String url_create_project = "http://10.0.2.2:8081/PrintBook/create_project.php";
 
 }
