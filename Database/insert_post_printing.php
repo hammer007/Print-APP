494883602a2e12e8da43b9d85f90c3ad79839451
Jobaer Ahmed <jobaer@student.chalmers.se>
2017-11-03 16:58:03 +0200

    Update insert_post_printing.php

--- Database/insert_post_printing.php
+++ Database/insert_post_printing.php
@@ -19,13 +19,13 @@
 
     require_once __DIR__ . '/db_connect.php';
     $db = new DB_CONNECT();
-    $insert_post_printing = "INSERT INTO printbook.post_printing(post_id, project_id, url_photo, support_removal, wedm, wedm_comment, blasting, blasting_time," +
-                          + "blasting_type, blasting_comment, stress_id, hardening_id, tempering_id, solution_id, aging_id," +
+    $insert_to_post_printing = "INSERT INTO printbook.post_printing(post_id, project_id, url_photo, support_removal, wedm, wedm_comment, blasting, blasting_time," +
+                          + "blasting_type, blasting_comment, stress_id, hardening_id, tempering_id, solution_id, aging_id)" +
                           + "VALUES('$post_id', '$project_id', '$url_photo', '$support_removal', '$wedm', '$wedm_comment', '$blasting', '$blasting_time'," +
-                          + "'$blasting_type', '$blasting_comment', '$stress_id', '$hardening_id', '$tempering_id', '$solution_id', '$aging_id',";
+                          + "'$blasting_type', '$blasting_comment', '$stress_id', '$hardening_id', '$tempering_id', '$solution_id', '$aging_id')";
 
     if(!$db) die('Something went wrong while connecting to database');
-    $result = mysqli_query($db->connect(), $insert_to_rinting) or die('unable to insert to printing');
+    $result = mysqli_query($db->connect(), $insert_to_post_printing) or die('unable to insert to printing');
     if ($result) {
         $response["success"] = 1;
         $response["message"] = "printing successfully inserted.";
