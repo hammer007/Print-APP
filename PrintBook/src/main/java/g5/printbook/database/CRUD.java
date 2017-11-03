fb64046bd79e0438ddf614c5b87fa04687ad4f24
Abely <abelandk@gmail.com>
2017-11-03 14:23:33 +0100

    database config refactored

--- PrintBook/src/main/java/g5/printbook/database/CRUD.java
+++ PrintBook/src/main/java/g5/printbook/database/CRUD.java
@@ -1,14 +1,9 @@
 package g5.printbook.database;
 
 import android.os.AsyncTask;
-import android.util.Log;
-
 import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,28 +13,19 @@ import java.util.List;
 public class CRUD extends AsyncTask<String, String, String> {
     Config config = new Config();
     JSONParser jsonParser = new JSONParser();
+    String do_what = config.TAG_CREATE_NOTHING;
+    List<NameValuePair> params;
+    public CRUD(List<NameValuePair> param, String do_what){
+        this.params = param;
+        this.do_what = do_what;
+    }
     @Override
     protected String doInBackground(String... strings) {
-        // Project Parameters
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("id", "1"));
-        params.add(new BasicNameValuePair("name", "test_1"));
-        JSONObject json = jsonParser.makeHttpRequest(config.url_create_project,
-                "POST", params);
-        try {
-            Log.i("tagconvertstr", "["+json+"]");
-            int success = json.getInt(config.TAG_SUCCESS);
-
-            if (success == 1) {
-                // successfully created project
-                System.out.print("success");
-            } else {
-                // failed to create project
-                System.out.print("fail");
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        int success = -1;
+        if(do_what == config.TAG_CREATE_PROJECT)  success = insertToDB(config.url_create_project);
+        else if (do_what == config.TAG_INSERT_TO_PREPRINTING) success = insertToDB(config.url_insert_preprinting);
+        if (success == 1) System.out.print("success");
+        else System.out.print("fail");
         return null;
     }
 
@@ -47,9 +33,18 @@ public class CRUD extends AsyncTask<String, String, String> {
     protected void onPreExecute() {
         super.onPreExecute();
     }
-    protected void onPostExecute(String file_url) {
-        // dismiss the dialog after getting all products
-
+    protected void onPostExecute(String file_url) {}
+    private int insertToDB(String doWhat){
+        System.out.print(" params " +params);
+        JSONObject json = jsonParser.makeHttpRequest(doWhat, "POST", params);
+        int success = -1;
+        try {
+            System.out.print( "" + json);
+            success = json.getInt(config.TAG_SUCCESS);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return success;
     }
 
 }
