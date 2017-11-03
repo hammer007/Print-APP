7141fabaccb9d9f7e7228ecc42f0e694baaeae63
Abely <abelandk@gmail.com>
2017-11-03 14:23:33 +0100

    database config refactored

--- PrintBook/src/main/java/g5/printbook/database/Config.java
+++ PrintBook/src/main/java/g5/printbook/database/Config.java
@@ -5,7 +5,10 @@ package g5.printbook.database;
  */
 
 public class Config {
+    protected static final String TAG_CREATE_PROJECT = "create_project";
+    protected static final String TAG_CREATE_NOTHING = "nothing";
     protected static final String TAG_SUCCESS = "success";
     protected static final String url_create_project = "http://10.0.2.2:8081/PrintBook/create_project.php";
-
+    protected static final String url_insert_preprinting = "http://10.0.2.2:8081/PrintBook/insert_printing.php";
+    protected static final String TAG_INSERT_TO_PREPRINTING = "insert_to_preprinting";
 }
