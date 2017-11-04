45231c3196fe971fbd239495cd44d1289e3cfae7
Abely <abelandk@gmail.com>
2017-11-04 01:15:46 +0100

    database files refactored

--- Database/insert_pre_printing.php
+++ Database/insert_pre_printing.php
@@ -5,14 +5,12 @@
     $project_id = $_POST['project_id'];
     $build_id = $_POST['build_id'];
     $no_parts = $_POST['no_parts'];
-	$printing_parameter = $_POST['printing_parameter'];
+	  $printing_parameter = $_POST['printing_parameter'];
     $comment = $_POST['comment'];
-	
+
     require_once __DIR__ . '/db_connect.php';
     $db = new DB_CONNECT();
-    $insert_to_pre_printing = "INSERT INTO printbook.pre_printing(pre_printing_id, project_id, build_id, no_parts, printing_parameter, comment)" +
-                          + "VALUES('$pre_printing_id', '$project_id', '$build_id', '$no_parts', '$printing_parameter', '$comment')";
-
+    $insert_to_pre_printing = "INSERT INTO printbook.pre_printing(pre_printing_id, project_id, build_id, no_parts, printing_parameter, comment) VALUES('$pre_printing_id', '$project_id', '$build_id', '$no_parts', '$printing_parameter', '$comment')";
     if(!$db) die('Something went wrong while connecting to database');
     $result = mysqli_query($db->connect(), $insert_to_pre_printing) or die('unable to insert to pre_printing');
     if ($result) {
