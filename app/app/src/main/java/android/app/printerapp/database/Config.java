3f3ec226295ce6065dc97c68df89f996991df32c
Abely <abelandk@gmail.com>
2017-11-24 11:50:59 +0100

    resseting two

---
+++ app/app/src/main/java/android/app/printerapp/database/Config.java
@@ -0,0 +1,78 @@
+package android.app.printerapp.database;
+
+/**
+ * Created by Geek on 10/21/2017.
+ */
+
+public class Config {
+    public static final String PREPRINTING_pre_printing_id = "pre_printing_id";
+    public static final String PREPRINTING_project_id = "project_id";
+    public static final String PREPRINTING_build_id = "build_id";
+    public static final String PREPRINTING_no_parts = "no_parts";
+    public static final String PREPRINTING_printing_parameter = "printing_parameter";
+    public static final String PREPRINTING_comment = "comment";
+    public static final String PRINTING_printing_id = "printing_id";
+    public static final String PRINTING_slm_id = "slm_id";
+    public static final String PRINTING_start_time = "start_time";
+    public static final String PRINTING_end_time = "end_time";
+    public static final String PRINTING_date = "date";
+    public static final String PRINTING_operator = "operator";
+    public static final String PRINTING_machine_type = "machine_type";
+    public static final String PRINTING_powder_weight_start = "powder_weight_start";
+    public static final String PRINTING_powder_weight_end = "powder_weight_end";
+    public static final String PRINTING_powder_waste_weight = "powder_waste_weight";
+    public static final String PRINTING_powder_used = "powder_used";
+    public static final String PRINTING_material_id = "material_id";
+    public static final String PRINTING_build_platform_weight = "build_platform_weight";
+    public static final String PRINTING_print_time = "print_time";
+    public static final String PRINTING_powder_condition = "powder_condition";
+    public static final String PRINTING_reused_times = "reused_times";
+    public static final String PRINTING_number_of_layers = "number_of_layers";
+    public static final String PRINTING_dpc_factor = "dpc_factor";
+    public static final String PRINTING_exposure_time = "exposure_time";
+    public static final String PRINTING_comments = "comments";
+    public static final String POSTPRINTING_slm_id = "slm_id" ;
+    public static final String POSTPRINTING_url_phto = "url_photo" ;
+    public static final String POSTPRINTING_support_removal = "support_removal" ;
+    public static final String POSTPRINTING_wedm = "wedm" ;
+    public static final String POSTPRINTING_wedm_comment = "wedm_comment" ;
+    public static final String POSTPRINTING_blasting = "blasting" ;
+    public static final String POSTPRINTING_blasting_time = "blasting_time" ;
+    public static final String POSTPRINTING_blasting_type = "blasting_type" ;
+    public static final String POSTPRINTING_blasting_comment = "blasting_comment" ;
+    public static final String POSTPRINTING_stress_id = "stress_id";
+    public static final String POSTPRINTING_hardening_id = "hardening_id";
+    public static final String POSTPRINTING_tempering_id = "tempering_id";
+    public static final String POSTPRINTING_solution_id = "solution_id";
+    public static final String POSTPRINTING_aging_id = "aging_id";
+    public static final String PROJECT_number = "project_number";
+    public static final String PROJECT_name = "project_name";
+    public static final String PROJECT_slm_id = "slm_id";
+    public static final String LOGIN_username = "username";
+    public static final String LOGIN_password = "password";
+    public static final String SIGNUP_first_name = "first_name";
+    public static final String SIGNUP_last_name = "last_name";
+    public static final String SIGNUP_user_name = "user_name";
+    public static final String SIGNUP_email_address = "email_address";
+    public static final String SIGNUP_password_name = "password";
+    public static final String SIGNUP_retype_password_name = "retype_password";
+    public static final String TAG_INSERT_SIGNUP = "insert_sign_up";
+    public static final String TAG_INSERT_PREPRINTING = "insert_pre_printing";
+    public static final String TAG_INSERT_PRINTING = "insert_printing";
+    public static final String TAG_INSERT_POSTPRINTING = "insert_post_printing";
+    public static final String TAG_CREATE_PROJECT = "create_project";
+    public static final String TAG_CREATE_NOTHING = "nothing";
+    public static final String TAG_SUCCESS = "success";
+    public static final String TAG_INSERT_ACCESS = "insert_access";
+    public static final String url_create_project = "https://group5sep.000webhostapp.com/create_project.php";//"http://10.0.2.2:8081/PrintBook/create_project.php";
+    public static final String url_insert_preprinting = "https://group5sep.000webhostapp.com/insert_pre_printing.php";
+    public static final String url_insert_printing = "https://group5sep.000webhostapp.com/insert_printing.php";
+    public static final String url_insert_post_printing = "https://group5sep.000webhostapp.com/insert_post_printing.php";
+    public static final String url_login = "https://group5sep.000webhostapp.com/login.php";
+    public static final String ServerUploadPath = "https://group5sep.000webhostapp.com/upload_magic_to_server.php" ;
+    public static final String url_search = "https://group5sep.000webhostapp.com/search.php";
+    public static final String url_insert_sign_up = "https://group5sep.000webhostapp.com/insert_sign_up.php";
+    public static final String url_get_users = "https://group5sep.000webhostapp.com/users.php";
+    public static final String url_insert_access = "https://group5sep.000webhostapp.com/insert_access_table.php";
+
+}
---
+++ app/app/src/main/java/android/app/printerapp/database/Insert.java
@@ -0,0 +1,51 @@
+package android.app.printerapp.database;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Geek on 10/21/2017.
+ */
+
+public class Insert extends AsyncTask<String, String, Integer> {
+    int success = -1;
+    Config config = new Config();
+    JSONParser jsonParser = new JSONParser();
+    String do_what = config.TAG_CREATE_NOTHING;
+    List<NameValuePair> params;
+    public Insert(List<NameValuePair> param, String do_what){
+        this.params = param;
+        this.do_what = do_what;
+    }
+    @Override
+    protected Integer doInBackground(String... strings) {
+        if(do_what == config.TAG_CREATE_PROJECT)  success = insertToDB(config.url_create_project);
+        else if (do_what == config.TAG_INSERT_PREPRINTING) success = insertToDB(config.url_insert_preprinting);
+        else if (do_what == config.TAG_INSERT_PRINTING) success = insertToDB(config.url_insert_printing);
+        else if (do_what == config.TAG_INSERT_SIGNUP) success = insertToDB(config.url_insert_sign_up);
+        else if (do_what == config.TAG_INSERT_POSTPRINTING) success = insertToDB(config.url_insert_post_printing);
+        return success;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+    protected int onPostExecute(int success) {return success;}
+    private int insertToDB(String doWhat){
+        JSONObject json = jsonParser.makeHttpRequest(doWhat, "POST", params);
+        try {
+            success = json.getInt(config.TAG_SUCCESS);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
+}
