d4caa3bdfcdf4bd9561e7bb38503173b2b98f6b5
Lídia Nyman <lidia.rpo@gmail.com>
2017-11-30 00:36:29 +0100

    Fixed the layout and replaced the upload stl model and the stl model

--- app/src/main/java/android/app/printerapp/MainFragment.java
+++ app/src/main/java/android/app/printerapp/MainFragment.java
@@ -1,5 +1,6 @@
 package android.app.printerapp;
 
+import android.app.printerapp.database.Search;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,11 +10,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
-    Button _3d,createMaterial_button;
+    Button _3d,createMaterial_button, newPrintJob_button, search2_button;
     View view;
     public MainFragment() {
         // Required empty public constructor
@@ -25,14 +27,19 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_main, container, false);
+
         _3d = (Button)view.findViewById(R.id.visualize_button);
         createMaterial_button = (Button)view.findViewById(R.id.createMaterial_button);
+        newPrintJob_button = (Button)view.findViewById(R.id.newPrintJob_button);
+        search2_button = (Button)view.findViewById(R.id.search2_button);
+
         _3d.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 callMain();
             }
         });
+
         createMaterial_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,6 +47,38 @@ public class MainFragment extends Fragment {
                 startActivity(mainIntent);
             }
         });
+
+        newPrintJob_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(v.getId() == R.id.newPrintJob_button){
+
+                    NewProjectMandatoryFragment fragment = new NewProjectMandatoryFragment();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction =
+                            getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+
+
+        search2_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(v.getId() == R.id.search2_button){
+
+                    SearchFragment fragment = new SearchFragment();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction =
+                            getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+
         return view;
     }
 
